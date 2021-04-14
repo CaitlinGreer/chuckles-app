2d5c0eb0f5aebce0d07c7059742bc751899edf75
CaitlinGreer <clento1121@gmail.com>
2021-04-14 13:16:35 -0400

    added heading to form

--- src/AddJoke/AddJoke.js
+++ src/AddJoke/AddJoke.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react'
 
+import './AddJoke.css'
+
 class AddJoke extends Component {
     render() {
         return (
-            <div>
+            <div className='form-page-container'>
+                <div className='form-heading'>
+                    <h2>Share A Chuckle...</h2>
+                </div>
                 <form>
                     <fieldset className='add-joke-fieldset'>
                         <legend>Tell Us A Joke</legend>
@@ -16,7 +21,7 @@ class AddJoke extends Component {
                             placeholder='Why did the chicken cross the road?'
                             required
                         />
-
+                        <br />
                         <label htmlFor='punchline'>Punchline:</label>
                         <input 
                             type='text'
@@ -25,7 +30,7 @@ class AddJoke extends Component {
                             placeholder='To get to the other side'
                             required
                         />
-
+                        <br />
                         <label htmlFor='user-name'>Submitted By:</label>
                         <input 
                             type='text'
@@ -33,7 +38,7 @@ class AddJoke extends Component {
                             name='punchline'
                             placeholder='Jerry Seinfeld'
                         />   
-                       
+                        <br />
                         <button 
                             type='submit'
                             className='submit-btn'>
