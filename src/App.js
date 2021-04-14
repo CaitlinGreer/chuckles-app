fae8d4e5c260f3e61899243e4a250709aca2bcb3
CaitlinGreer <clento1121@gmail.com>
2021-04-14 10:01:42 -0400

    added basic layout

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react'
+import { Route, Link } from 'react-router-dom'
+import './App.css'
+import Nav from './Nav/Nav' //Navigation Bar
 
-function App() {
-  return (
-    <main className='App'>
-      {/* content goes here */}
-    </main>
-  );
+class App extends Component {
+  render(){
+    return (
+      <main className='App'>
+        <Nav />
+        <header className='header'>
+          <h1>Chuckles</h1>
+          <h2>Where Dad Jokes Are Our Specialty</h2>
+        </header>
+        <Route
+              exact
+              path='/'
+              component={LandingPage}
+            />
+            <Route
+              path='/add-joke'
+              component={AddJoke}
+            />
+            <Route
+              path='/jokes-list'
+              component={JokesList}
+            />
+      </main>
+    );
+  }
 }
-
 export default App;
