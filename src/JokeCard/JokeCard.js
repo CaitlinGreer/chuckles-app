cb709204d969d9c6a6ca8e169c1c8a0060d9136b
CaitlinGreer <clento1121@gmail.com>
2021-04-14 10:21:00 -0400

    initial setup for JokeCard

---
+++ src/JokeCard/JokeCard.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react'
+
+class JokeCard extends Component {
+    render() {
+        return (
+            <div>
+                <div class='user-joke'>
+                    <p>Placeholder for joke here</p>
+                    <p>Placeholder for hilarious punchline</p>
+                    <p>Submitted By...</p>
+                </div>
+                <div class='user-joke'>
+                    <p>Placeholder for joke here</p>
+                    <p>Placeholder for hilarious punchline</p>
+                    <p>Submitted By...</p>
+                </div>
+                <div class='user-joke'>
+                    <p>Placeholder for joke here</p>
+                    <p>Placeholder for hilarious punchline</p>
+                    <p>Submitted By...</p>
+                </div>
+                <div class='user-joke'>
+                    <p>Placeholder for joke here</p>
+                    <p>Placeholder for hilarious punchline</p>
+                    <p>Submitted By...</p>
+                </div>
+                <div class='user-joke'>
+                    <p>Placeholder for joke here</p>
+                    <p>Placeholder for hilarious punchline</p>
+                    <p>Submitted By...</p>
+                </div>
+            </div>
+            
+        )
+    }
+}
+
+export default JokeCard;
