95cf9a779d4e3bee09fa8480013f013de1213ea1
CaitlinGreer <clento1121@gmail.com>
2021-04-14 11:10:10 -0400

    created footer

---
+++ src/Footer/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class Footer extends React.Component {
+    render() {
+        return (
+            <div className='footer'>
+                <p>Created By Caitlin Greer</p>
+            </div>
+        )
+    }
+}
+
+export default Footer;
