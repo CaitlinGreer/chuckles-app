b2c5cb1be0e6c0ffd7aec36930d1c9acfaa520a7
CaitlinGreer <clento1121@gmail.com>
2021-04-14 10:21:41 -0400

    created JokesList

---
+++ src/JokesList/JokesList.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import JokeCard from '../JokeCard/JokeCard'
+import './JokesList.css'
+
+class JokesList extends Component {
+    render() {
+        return (
+            <JokeCard />
+        )
+    }
+}
+
+export default JokesList;
