32169120eaaa359be239d72eff98b838deb604d5
CaitlinGreer <clento1121@gmail.com>
2021-04-14 11:10:23 -0400

    added className to links

--- src/Nav/Nav.js
+++ src/Nav/Nav.js
@@ -1,14 +1,15 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import './Nav.css'
 
 export default function Nav(props) {
     return (
         <nav className='Nav'>
-          <Link to={'/'}>Home</Link>
+          <Link className='nav-link' to={'/'}>Home</Link>
           {' '}
-          <Link to={'/jokes-list'}>Jokes</Link>
+          <Link className='nav-link' to={'/jokes-list'}>Jokes</Link>
           {' '}
-          <Link to={'/add-joke'}>Tell Us A Joke</Link>
+          <Link className='nav-link' to={'/add-joke'}>Tell Us A Joke</Link>
         </nav>
     )
 }
