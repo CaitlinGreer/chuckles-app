ca072dd1036e5abb311742c2ec4e8dcb95ce2e70
CaitlinGreer <clento1121@gmail.com>
2021-04-14 13:16:48 -0400

    input fake data

--- src/JokeCard/JokeCard.js
+++ src/JokeCard/JokeCard.js
@@ -1,33 +1,35 @@
 import React, { Component } from 'react'
 
+import './JokeCard.css'
+
 class JokeCard extends Component {
     render() {
         return (
             <div>
-                <div class='user-joke'>
-                    <p>Placeholder for joke here</p>
-                    <p>Placeholder for hilarious punchline</p>
-                    <p>Submitted By...</p>
+                <div className='user-joke'>
+                    <p className='joke'>What did the custodian say when he jumped out of the closet?</p>
+                    <p className='punchline'>Supplies!!!</p>
+                    <p className='user-name'>Submitted By: </p>
                 </div>
-                <div class='user-joke'>
-                    <p>Placeholder for joke here</p>
-                    <p>Placeholder for hilarious punchline</p>
-                    <p>Submitted By...</p>
+                <div className='user-joke'>
+                    <p className='joke'>What do sprinters eat before a race?</p>
+                    <p className='punchline'>Nothing.  They fast.</p>
+                    <p className='user-name'>Submitted By:</p>
                 </div>
-                <div class='user-joke'>
-                    <p>Placeholder for joke here</p>
-                    <p>Placeholder for hilarious punchline</p>
-                    <p>Submitted By...</p>
+                <div className='user-joke'>
+                    <p className='joke'>What did the pirate say on his 80th birthday?</p>
+                    <p className='punchline'>"Aye, matey!"</p>
+                    <p className='user-name'>Submitted By: </p>
                 </div>
-                <div class='user-joke'>
-                    <p>Placeholder for joke here</p>
-                    <p>Placeholder for hilarious punchline</p>
-                    <p>Submitted By...</p>
+                <div className='user-joke'>
+                    <p className='joke'>How do you organize a party in space?</p>
+                    <p className='punchline'>You planet</p>
+                    <p className='user-name'>Submitted By:</p>
                 </div>
-                <div class='user-joke'>
-                    <p>Placeholder for joke here</p>
-                    <p>Placeholder for hilarious punchline</p>
-                    <p>Submitted By...</p>
+                <div className='user-joke'>
+                    <p className='joke'>What kinds of pictures do hermit crabs take?</p>
+                    <p className='punchline'>Shellfies</p>
+                    <p className='user-name'>Submitted By:</p>
                 </div>
             </div>
             
