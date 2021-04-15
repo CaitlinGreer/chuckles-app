8917883309dfb5205cec411a455f4865f370805d
CaitlinGreer <clento1121@gmail.com>
2021-04-15 10:01:47 -0400

    added props for dummy data

--- src/JokeCard/JokeCard.js
+++ src/JokeCard/JokeCard.js
@@ -1,40 +1,20 @@
-import React, { Component } from 'react'
+import React from 'react'
 
 import './JokeCard.css'
 
-class JokeCard extends Component {
-    render() {
+function JokeCard(props){
+    console.log(props)
         return (
             <div>
                 <div className='user-joke'>
-                    <p className='joke'>What did the custodian say when he jumped out of the closet?</p>
-                    <p className='punchline'>Supplies!!!</p>
-                    <p className='user-name'>Submitted By: </p>
-                </div>
-                <div className='user-joke'>
-                    <p className='joke'>What do sprinters eat before a race?</p>
-                    <p className='punchline'>Nothing.  They fast.</p>
-                    <p className='user-name'>Submitted By:</p>
-                </div>
-                <div className='user-joke'>
-                    <p className='joke'>What did the pirate say on his 80th birthday?</p>
-                    <p className='punchline'>"Aye, matey!"</p>
-                    <p className='user-name'>Submitted By: </p>
-                </div>
-                <div className='user-joke'>
-                    <p className='joke'>How do you organize a party in space?</p>
-                    <p className='punchline'>You planet</p>
-                    <p className='user-name'>Submitted By:</p>
-                </div>
-                <div className='user-joke'>
-                    <p className='joke'>What kinds of pictures do hermit crabs take?</p>
-                    <p className='punchline'>Shellfies</p>
-                    <p className='user-name'>Submitted By:</p>
+                    <p className='joke'>{props.joke}</p>
+                    <p className='punchline'>{props.punchline}</p>
+                    <p className='user-name'>Submitted By: {props.submitted_by}</p>
                 </div>
             </div>
             
         )
     }
-}
+
 
 export default JokeCard;
--- src/JokesList/JokesList.js
+++ src/JokesList/JokesList.js
@@ -1,14 +1,26 @@
 import React, { Component } from 'react'
 import JokeCard from '../JokeCard/JokeCard'
 import './JokesList.css'
+import dummyJokes from '../DummyJokes'
 
 class JokesList extends Component {
+      
     render() {
+        const jokeCards = dummyJokes.map(joke => {
+            return (
+                <JokeCard 
+                    key={joke.id} 
+                    joke={joke.joke}
+                    punchline={joke.punchline}
+                    submitted_by={joke.submitted_by}
+                />
+            )
+        })
         return (
             <div>
                 <h2 className='joke-list-heading'>Let's Have A Chuckle...</h2>
             <div className='jokes-container'>
-                <JokeCard />
+                {jokeCards}
             </div>
             </div>
         )
