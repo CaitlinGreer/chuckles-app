d2f4befd06f5e50688bb19673e086e46bafb500f
CaitlinGreer <clento1121@gmail.com>
2021-04-15 10:01:13 -0400

    created dummy data

---
+++ src/DummyJokes.js
@@ -0,0 +1,28 @@
+const dummyJokes = [
+    {
+        id: 1,
+        joke: 'What did the custodian say when he jumped out of the closet?',
+        punchline: 'Supplies!!!',
+        submitted_by: 'John Doe',
+    },
+    {
+        id: 2,
+        joke: 'What do sprinters eat before a race?',
+        punchline: 'Nothing.  They fast.',
+        submitted_by: 'Jane Doe',
+    },
+    {
+        id: 3,
+        joke: 'What did the pirate say on his 80th birthday?',
+        punchline: '"Aye, matey!"',
+        submitted_by: 'Jack Sparrow',
+    },
+    {
+        id: 4,
+        joke: 'How do you organize a party in space?',
+        punchline: 'You planet',
+        submitted_by: 'Neil Armstrong',
+    },
+]
+
+export default dummyJokes;
