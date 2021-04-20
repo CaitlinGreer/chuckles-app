83822aa643f0725234706aff47f7f77baea43371
CaitlinGreer <clento1121@gmail.com>
2021-04-20 14:10:15 -0400

    reconfigured to attach to server

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
 import { Route, Switch } from 'react-router-dom'
+import JokesContext from './jokesContext'
+import config from './config'
 import './App.css'
 
 import Nav from './Nav/Nav' //Navigation Bar
@@ -9,10 +11,47 @@ import AddJoke from './AddJoke/AddJoke' //add a joke form
 
 
 class App extends Component {
-  render(){
+  constructor(props) {
+    super(props)
+      this.state = {
+        jokes: [],
+        error: null,
+        loading: false,
+      }
+  }
+
+  componentDidMount() {
+    this.fetchAllData()
+  }
+
+  fetchAllData = () => {
+    Promise.all([
+      this.fetchJokes()
+    ])
+      .then(([jokes]) => {
+        this.setState({
+          jokes
+        })
+      })
+      .catch(error => {
+        this.setState({error})
+      })
+  }
+
+  fetchJokes = () => {
+    return fetch(`${config.API_ENDPOINT}/jokes`)
+      .then(res => res.json())
+  }
+
+  addJokes = joke => {
+    this.setState({
+      jokes: [...this.state.jokes, joke]
+    })
+  }
+
+  renderRoutes(){
     return (
       <div className='App'>
-        <Nav />
         <Switch>       
           <Route
               exact
@@ -30,6 +69,25 @@ class App extends Component {
         </Switch>
       </div>
     );
+  }
+
+  render() {
+    const value = {
+      jokes: this.state.jokes,
+      fetchJokes: this.fetchAllData,
+    }
+    return (
+      <JokesContext.Provider value={value}>
+        <div>
+          <nav>
+            <Nav />
+          </nav>
+          <main>
+            {this.renderRoutes()}
+          </main>
+        </div>
+      </JokesContext.Provider>
+    )
   }
 }
 export default App;
