337ba378e57729f9c79f014debfd6858b74f7a61
CaitlinGreer <clento1121@gmail.com>
2021-04-27 16:56:50 -0400

    added more styling

--- src/LandingPage/LandingPage.js
+++ src/LandingPage/LandingPage.js
@@ -14,18 +14,23 @@ class LandingPage extends Component {
             
                 <div className='landing-page-container' id='landing-page-container'>
                     <div className='welcome-info'>
-                        <p>Did you know laughter and smiling are scientifically proven to improve your health and mental state?  I know...it's crazy right? 
-                           With a world as crazy as it is, why not create something that brings a little joy into people's lives!
+                        <h3>A Little About Chuckles!</h3>
+                        <p>Did you know laughter and smiling are scientifically proven to improve your health and mental state?  That means laughter really is the best medicine!  I know...it's crazy right? 
+                           <br /> <br />
+                           Every day we're surrounded by humans who take life too seriously.  Chuckles is here to do the exact opposite!
                         </p>
                         <p>
                            Chuckles is here to turn a bad day into a good day and a good day into a great day.  So turn that frown upside down and lets get to chuckling!
                         </p>
-                        
+                    </div> 
+                    <div className='share-a-joke'>
                         <p>If you're feeling adventurous you may even share a joke or two right <Link to={'/add-joke'}>here</Link>! Don't worry we keep them anonymous, we won't let
                            anyone know how much you love tacky, corny punny nonsense
                         </p>
+                    </div>
+                        
 
-                    </div>   
+                      
 
                     <hr />
 
